b8061f85b54c04849d22bd5eee985c5091d25d46
Hecnj79 <112528928+Hecnj79@users.noreply.github.com>
2024-02-27 15:55:39 -0800

    Add files via upload

---
+++ Scripts/HealthEnemy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthEnemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
+    public float rotateSpeed = 0.25f;
+    public float health = 3f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!target)
+        {
+            GetTarget();
+        }
+        else
+        {
+            RotateTowardsTarget();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    private void RotateTowardsTarget()
+    {
+        Vector2 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
+    }
+
+    private void GetTarget()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            LevelManager.manager.GameOver();
+            Destroy(collision.gameObject);
+            target = null;
+        }
+        else if (collision.gameObject.CompareTag("Bullet"))
+        {
+            health -= 1;
+            Destroy(collision.gameObject);
+            if (health == 0)
+            {
+                LevelManager.manager.IncreaseScore(2);
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    //Gun Variables
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    [Range(0.1f, 2f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+    private Rigidbody2D rb;
+    private float mx;
+    private float my;
+
+    private float fireTimer;
+
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        //for machine gun and shoot just by holding down, Input.GetMouseButton(0).....
+        if(Input.GetMouseButtonDown(0) && fireTimer <= 0f)
+        {
+            Shoot();
+            fireTimer = fireRate;
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx, my).normalized * speed;
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyBullet"))
+        {
+            LevelManager.manager.GameOver();
+            Destroy(gameObject);
+        }
+    }
+}
